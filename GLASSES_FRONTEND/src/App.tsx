bc714a7b5062bf360d7c813374b436649e1de272
Mostafa <s-mostafa.abdelhameed@zewailcity.edu.eg>
2024-11-29 15:39:18 +0200

    home added

--- GLASSES_FRONTEND/src/App.tsx
+++ GLASSES_FRONTEND/src/App.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LoginPage from "./message"; // Your Login Page
 import SignupPage from "./Signup"; // Your Signup Page
+import Home from "./Home";
 
 const App: React.FC = () => {
   return (
@@ -10,6 +11,7 @@ const App: React.FC = () => {
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </Router>
   );
