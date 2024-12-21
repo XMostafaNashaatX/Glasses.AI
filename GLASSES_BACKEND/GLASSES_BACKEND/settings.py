d17514777acac6b61dd773e4894766b668ffad11
AhmedADesoky <s-ahmed.desoky@zewailcity.edu.eg>
2024-12-21 14:17:00 +0200

    Updatded-connection

--- GLASSES_BACKEND/GLASSES_BACKEND/settings.py
+++ GLASSES_BACKEND/GLASSES_BACKEND/settings.py
@@ -201,7 +201,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.mysql",
         "NAME": "glasses_db",
         "USER": "root",
-        "PASSWORD": "abdo1234",
+        "PASSWORD": "1234",
         "HOST": "127.0.0.1",
         "PORT": "3306",
     }
